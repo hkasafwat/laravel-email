449381635819487ba14dea39f7baff8deb4c0293
hisham <hkasafwat@gmail.com>
2021-09-13 17:22:23 +0100

    laravel setup

---
+++ resources/views/livewire/email-form.blade.php
@@ -0,0 +1,9 @@
+<div>
+    <div class="bg-purple-200 shadow rounded p-4">
+        <div>Send Email</div>
+
+        <form>
+            <input type="text" name=""
+        </form>
+    </div>
+</div>
